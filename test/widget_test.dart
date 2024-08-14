9396fd1ae4163e32613bc76f76c5fef5a3c2ef56
prince-342 <prince.s@marketsimplified.com>
2024-08-14 18:58:26 +0530

    wanrnigs removed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,21 +7,18 @@
 
 
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todoapp/main.dart';
+
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-  //
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-  //
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
+  testWidgets('MyApp initializes correctly and builds the correct MaterialApp', (WidgetTester tester) async {
+    // Build the widget tree.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify the title of the app.
+    expect(find.text('Flutter Demo'), findsNothing); // Title is not visible in the UI but it's set in MaterialApp.
+
+    // Extract the MaterialApp widget to inspect its properties.
+  });
+
 }
