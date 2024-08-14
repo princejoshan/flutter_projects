c8f99f6de8fdfe394db6ed3c56ec7a6608c36949
prince-342 <prince.s@marketsimplified.com>
2024-08-14 14:13:17 +0530

    github action added

--- todoapp/lib/utilities/appcolors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryColor = Colors.blueAccent;
-  static const Color white = Colors.white;
-  static const Color purple = Colors.purpleAccent;
-
-}
